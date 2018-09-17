1f00e0a6921c8181c53f66533a6b3821afa342b6
MajaSveljo <majasveljo@gmail.com>
2018-09-17 15:45:38 +0200

    events

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <app-header v-bind:title="title" v-on:changeTitle="updateTitle($event)"></app-header>
+    <app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
+    <app-footer v-bind:title="title"></app-footer>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+import Ninjas from './components/Ninjas.vue'
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    'app-header': Header,
+    'app-footer': Footer,
+    'app-ninjas': Ninjas
+  },
+  data() {
+    return {
+      ninjas: [
+            {name: 'Maja', specialty: 'Ninja', show: 'false'},
+            {name: 'Dragan', specialty: 'Fat', show: 'false'},
+            {name: 'Jovana', specialty: 'Pretty', show: 'false'},
+            {name: 'Milan', specialty: 'Smart', show: 'false'},
+            {name: 'Mia', specialty: 'Elegant', show: 'false'},
+        ],
+
+      title: 'Vue Ninjas'
+    }
+  },
+  methods: {
+    updateTitle: function(updatedTitle) {
+      this.title = updatedTitle;
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
+
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+
+      <footer>
+          <p>{{ copyright }}{{ title }}</p>
+      </footer>
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  props: {
+      title: String
+  },
+  
+  components: {
+     
+  },
+  data() {
+    return {
+      copyright: 'Copyright 2017 Vue Ninjas'
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+    footer{
+        background: #222;
+        padding: 6px;
+    }
+
+    p{
+        color: lightgreen;
+        text-align: center;
+    }
+
+</style>
