8bb2bdd03594c2f72fd3443799dd4eb237774156
MajaSveljo <majasveljo@gmail.com>
2018-09-18 14:03:43 +0200

    slots - example - form schema for multiple forms

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,19 @@
   <div>
     
     <formHelper>
-      <h2 slot="title">{{ title }}</h2>
-      <p slot="text">I am the paragraph text for the slot</p>
+      <div slot="form-header">
+        <h3>This is the title of the form</h3>
+        <p>Information about the form</p>
+      </div>
+      <div slot="form-fields">
+        <input type="text" placeholder="name" required />
+        <input type="password" placeholder="password" required />
+      </div>
+      <div slot="form-controls">
+        <!-- handleSubmit function isn't defined in methods, it was just written for
+        practicing purpose -->
+        <button v-on:click="handleSubmit">Submit</button>
+      </div>
     </formHelper>
 
   </div>
--- src/components/formHelper.vue
+++ src/components/formHelper.vue
@@ -1,8 +1,29 @@
 <template>
+
+    <!-- with this kind of organization and slot-using we are enableing the developer to
+    use the same form(schema) for multiple forms(questinares) by having the structure
+    and just editing slots content. Also they styling would be done in this component below -->
   <div>
-    <slot name="title" />
-    <h1>I am the form helper</h1>
-    <slot name="text" />
+    <h1>Please fill out our form</h1>
+    <form>
+        <div id ="form-header">
+            <slot name="form-header" />
+        </div>
+        <div id="form-fields">
+            <slot name="form-fields" />
+        </div>
+        <div id="form-controls">
+            <slot name="form-controls" />
+        </div>
+        <div id="useful-links">
+            <ul>
+                <li> <a href="#">Link 1</a></li>
+                <li> <a href="#">Link 2</a></li>
+                <li> <a href="#">Link 3</a></li>
+                <li> <a href="#">Link 4</a></li>
+            </ul>
+        </div>
+    </form>
   </div>
 </template>
 
@@ -25,8 +46,6 @@ export default {
 
 <style scoped>
 
-h1{
-    color: red;
-}
+
 
 </style>
