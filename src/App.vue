c167c86f3798e921d9b3d071d898834788c983d4
MajaSveljo <majasveljo@gmail.com>
2018-09-18 15:23:09 +0200

    Dynamic Components

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,26 @@
 <template>
   <div>
-    
-    <formHelper>
-      <div slot="form-header">
-        <h3>This is the title of the form</h3>
-        <p>Information about the form</p>
-      </div>
-      <div slot="form-fields">
-        <input type="text" placeholder="name" required />
-        <input type="password" placeholder="password" required />
-      </div>
-      <div slot="form-controls">
-        <!-- handleSubmit function isn't defined in methods, it was just written for
-        practicing purpose -->
-        <button v-on:click="handleSubmit">Submit</button>
-      </div>
-    </formHelper>
+    <keep-alive>
+      <component v-bind:is="component"></component>
+    </keep-alive>
+    <button v-on:click="component = 'form-one'">Show Form One</button>
+    <button v-on:click="component = 'form-two'">Show Form Two</button>
 
   </div>
 </template>
 
 <script>
-import formHelper from './components/formHelper.vue'
+import formOne from './components/formOne.vue'
+import formTwo from './components/formTwo.vue'
 
 export default {
   components: {
-    formHelper: formHelper
+    'form-one': formOne,
+    'form-two': formTwo
 },
   data() {
     return {
-      // if we're gonna add content dynamicaly the content MUST be defined in the component
-      // where the slots are called. In this example it's here
-      title: 'I am a dynamic slot title'
+      component: 'form-two'
     }
   },
   methods: {
