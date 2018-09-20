cbc5f342e7753dbacd4f715ff5f62d9de07bda35
MajaSveljo <majasveljo@gmail.com>
2018-09-20 20:23:36 +0200

    Route parameters with working links

---
+++ src/components/singleBlog.vue
@@ -0,0 +1,35 @@
+<template>
+
+<div id="single-blog">
+    <h1>{{blog.title}}</h1>
+    <article>{{blog.body}}</article>
+</div>
+
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            //we're acessing the route parameter in the id property
+            id: this.$route.params.id,
+            blog: {}
+        }
+    },
+    created() {
+        this.$http.get('http://jsonplaceholder.typicode.com/posts/' + this.id).then(function(data){
+            this.blog = data.body;
+        });
+    }
+}
+
+</script>
+
+<style>
+
+#singleBlog{
+    max-width: 960px;
+    margin: 0 auto;
+}
+
+</style>
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import showBlogs from './components/showBlogs.vue'
 import addBlog from './components/addBlog.vue'
+import singleBlog from './components/singleBlog.vue'
 
 //we'll export an array
 //each element in this array will reperesent a different route
@@ -9,7 +10,8 @@ export default [
     //e.g. www.mywebsiteisawesome.com/thepath
     //the path comes after the slash. son in this e.g. it's "thepath"
     {path: '/', component: showBlogs},
-    {path: '/add', component: addBlog}
+    {path: '/add', component: addBlog},
+    {path: '/blog/:id', component: singleBlog}
 ]
 
 //after this we need to export the routes and register them in the const router so
